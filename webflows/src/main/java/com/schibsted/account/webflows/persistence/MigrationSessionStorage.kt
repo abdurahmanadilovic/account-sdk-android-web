19dbdd5eb939007974e971c16dc2e893a40eaffc
artur.poplawski@schibsted.com <artur.poplawski@schibsted.com>
2023-05-18 09:13:28 +0200

    Clean up

--- webflows/src/main/java/com/schibsted/account/webflows/persistence/MigrationSessionStorage.kt
+++
@@ -1,6 +0,0 @@
-package com.schibsted.account.webflows.persistence
-
-import com.schibsted.account.webflows.user.StoredUserSession
-import com.schibsted.account.webflows.util.Either
-import timber.log.Timber
-
--- webflows/src/main/java/com/schibsted/account/webflows/persistence/SharedPrefsStorage.kt
+++
@@ -1,7 +0,0 @@
-package com.schibsted.account.webflows.persistence
-
-import android.content.Context
-import com.google.gson.GsonBuilder
-import com.schibsted.account.webflows.user.StoredUserSession
-import com.schibsted.account.webflows.util.Either
-import com.schibsted.account.webflows.util.Util.getStoredUserSession
